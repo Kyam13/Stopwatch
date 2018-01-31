f4df3a048e8e4b8613405c5c30010a2dd07c445b
kyam <kou.skillup@gmail.com>
2018-02-01 01:19:37 +0900

    make timer

--- app/src/main/java/com/timerapplication/stopwatch/MainActivity.kt
+++ app/src/main/java/com/timerapplication/stopwatch/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.os.Handler
+import android.media.RingtoneManager
 import java.security.KeyStore
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
     var timeValue = 0
     // startが偶数回押された時にstopになるようにするための判別変数
     var startchangeStop =false
+    //timerの時につける変数
+    var timerdecide =false
+    //timerstop
+    //var timerstop=false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +35,25 @@ class MainActivity : AppCompatActivity() {
         val minuteButton:Button = findViewById(R.id.min)
         val secondButton:Button = findViewById(R.id.sec)
 
+        // 現在設定されている着信音を選択する
+        val ringType = RingtoneManager.TYPE_NOTIFICATION
+        val soundUri = RingtoneManager.getActualDefaultRingtoneUri(this, ringType)
+        val ringtone = RingtoneManager.getRingtone(applicationContext, soundUri)
 
         //1秒ごとに処理を実行
+        //Runnableクラスに対する無名オブジェクト
         val runnable = object : Runnable{
-            override fun run(){
-                timeValue++
+            override fun run() {
+                if (timeValue>0 && timerdecide==true) {
+                    timeValue=timeValue.dec()
+                }
+//                else if(timeValue==0&&timerdecide==true){
+//                    timerdecide=false
+//                }
+                else{// if(timerstop==true){
+                    timerdecide=false
+                    timeValue=timeValue.inc()
+                }
                 // TextViewを更新
                 // ?.letを用いて、nullではない場合のみ更新
                 timeToText(timeValue)?.let {
@@ -45,22 +64,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
         // start and stop
         startandstopButton.setOnClickListener {
-            startchangeStop = if(startchangeStop==false) {
+            if(startchangeStop==false) {
                 handler.post(runnable)
-                true
+                startchangeStop =true
             }
             else {
                 handler.removeCallbacks(runnable)
-                false
+                startchangeStop = false
             }
+//            timerstop= if(timeValue==0){true }else{ false}
         }
 
-        // stop
-//        stopButton.setOnClickListener {
-//            handler.removeCallbacks(runnable)
-//        }
         // reset
         resetButton.setOnClickListener {
             handler.removeCallbacks(runnable)
@@ -71,32 +88,30 @@ class MainActivity : AppCompatActivity() {
             }
         }
         //hour
-        hourButton.setOnClickListener{
-            timeValue=timeValue+3600
+        hourButton.setOnClickListener {
+            timerdecide=true
+            timeValue +=3600
             timeToText(timeValue)?.let {
-                // timeToText(timeValue)の値がlet内ではitとして使える
                 timeText.text = it
             }
-            handler.postDelayed(this, 1000)
         }
         //minute
-        minuteButton.setOnClickListener{
-            timeValue=timeValue+60
+        minuteButton.setOnClickListener {
+            timeValue +=60
+            timerdecide=true
             timeToText(timeValue)?.let {
-                // timeToText(timeValue)の値がlet内ではitとして使える
                 timeText.text = it
             }
-            handler.postDelayed(this, 1000)
         }
         //second
-        secondButton.setOnClickListener{
-            timeValue=timeValue+1
+        secondButton.setOnClickListener {
+            timeValue +=1
+            timerdecide=true
             timeToText(timeValue)?.let {
-                // timeToText(timeValue)の値がlet内ではitとして使える
                 timeText.text = it
             }
-            handler.postDelayed(this, 1000)
         }
+//
     }
     // 数値を00:00:00形式の文字列に変換する関数
     // 引数timeにはデフォルト値0を設定、返却する型はnullableなString?型
